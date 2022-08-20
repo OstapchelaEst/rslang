52630a8cf3087e5bc187d86d9c6486f93056fe62
OstapchelaEst <Ostapchela@gmail.com>
2022-08-20 13:18:30 +0200

    fix: change render main page

---
+++ src/components/pages/main/block-text.ts
@@ -0,0 +1,69 @@
+export const TEAM_TEXT = {
+  artem: {
+    prevNameText: `Наш team-leader`,
+    name: `Артём`,
+    gitLink: `https://github.com/RockStar666by`,
+    gitImg: `https://avatars.githubusercontent.com/u/52502654?v=4`,
+    text: `
+      Занимался тем и тем Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia eveniet,
+      libero dignissimos autipsum dolor sit amet consectetur adipisicing elit. Quia eveniet,
+      libero dignissimos aut
+      `,
+  },
+  dima: {
+    prevNameText: `Front-end разработчик`,
+    name: "Дмитрий",
+    gitLink: `https://github.com/OstapchelaEst`,
+    gitImg: `https://avatars.githubusercontent.com/u/90386861?v=4`,
+    text: `            
+      Занимался тем и тем Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia eveniet,
+      libero dignissimos autipsum dolor sit amet consectetur adipisicing elit. Quia eveniet,
+      libero dignissimos aut`,
+  },
+  lena: {
+    prevNameText: `Front-end разработчик`,
+    name: `Елена`,
+    gitLink: `https://github.com/LenaStask`,
+    gitImg: "https://avatars.githubusercontent.com/u/96074310?v=4",
+    text: `
+      Занимался тем и тем Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia eveniet,
+      libero dignissimos autipsum dolor sit amet consectetur adipisicing elit. Quia eveniet,
+      libero dignissimos aut
+      `,
+  },
+  kirill: {
+    prevNameText: `Front-end разработчик`,
+    name: "Кирилл",
+    gitLink: `https://github.com/teame92`,
+    gitImg: `https://avatars.githubusercontent.com/u/50177907?v=4`,
+    text: `
+      Занимался тем и тем Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia eveniet,
+      libero dignissimos autipsum dolor sit amet consectetur adipisicing elit. Quia eveniet,
+      libero dignissimos aut
+      `,
+  },
+};
+
+export const FUNCTIONAL_TEXT = {
+  textbook: {
+    title: `Учебник`,
+    text: `    Более 3500 тысяч слов для изучения, разбитых на разделы по уровню твоей подготовки с удобной
+      навигацией.`,
+  },
+  statistics: {
+    title: `Статистика`,
+    text: `       
+      Отслеживай свой прогресс в индивидуальной статистике, ставь цели и вдохновляйся на и достижение
+      новых результатов каждый день!
+      `,
+  },
+  vocabulary: {
+    title: `Словарь`,
+    text: ` Создай свой персональный словарь для изучения слов - добавляй слова, которым хочешь уделить
+      особое внимание и удаляй, если слово тебе уже известно.`,
+  },
+  games: {
+    title: `Игры`,
+    text: `2 увлекательных игры на развитие запоминания слов и восприятия на слух.`,
+  },
+};
