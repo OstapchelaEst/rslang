8f5f00acf54164eb8d55ebb0706f74351e09f1a6
Kirill Volosevich <keriaz@mail.ru>
2022-08-28 21:14:25 +0300

    feat: sprint game functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,53 @@ import Navigo from "navigo";
 import { RENDER_BASIC_STRUCTURE } from "../pages/main/main-page-render";
 import { COMPONENT_HEADER } from "../components/header/header";
 import { COMPONENT_FOOTER } from "../components/footer/footer";
+import Sprint from "../pages/games/sprint/sprint";
+import GameResult from "../pages/games/gameResult/gameResult";
 
-const router = new Navigo("/");
+const router = new Navigo("/", { hash: true });
 
-router.on("", async () => {
-  COMPONENT_HEADER.createHeader();
-  RENDER_BASIC_STRUCTURE.buildMainPage();
-  COMPONENT_FOOTER.createFooter();
+router.on({
+  "/": async () => {
+    COMPONENT_HEADER.createHeader();
+    RENDER_BASIC_STRUCTURE.buildMainPage();
+    COMPONENT_FOOTER.createFooter();
+  },
+  "/sprint": async () => {
+    const container: HTMLElement | null =
+      document.querySelector(".main__container");
+
+    if (!container) return;
+    const contentURL = "https://rs-learnwords-example.herokuapp.com";
+    const gameResult: GameResult = new GameResult(contentURL);
+    const sprint: Sprint = new Sprint(contentURL, gameResult);
+
+    await sprint.createComponent();
+    await gameResult.createComponent();
+
+    COMPONENT_HEADER.createHeader();
+
+    container.innerHTML = "";
+    sprint.startGame(1);
+  },
 });
+
+// router.on("/sprint", async () => {
+//   const container: HTMLElement | null =
+//     document.querySelector(".main__container");
+
+//   if (!container) return;
+//   const contentURL = "https://rslang-app-2022.herokuapp.com/";
+//   const gameResult: GameResult = new GameResult(contentURL);
+//   const sprint: Sprint = new Sprint(contentURL, gameResult);
+
+//   await sprint.createComponent();
+//   await gameResult.createComponent();
+
+//   COMPONENT_HEADER.createHeader();
+
+//   container.innerHTML = "";
+//   sprint.startGame(1);
+//   // container.append(await sprint.createComponent());
+// });
 
 router.resolve();
---
+++ src/pages/games/gameResult/gameResult.ts
@@ -0,0 +1,376 @@
+import {
+  WordContent,
+  OptionalUserWord,
+  OptionalUserStatistics,
+  AuthorizationContent,
+} from "../../../interfaces/interfaceServerAPI";
+import usersWords from "../../../controller/services/usersWords";
+import statistic from "../../../controller/services/usersStatistic";
+import { LOCAL_STORAGE } from "../../../controller/local-storage/local-storage";
+
+export default class GameResult {
+  innerHtmlTemplate = `
+    <div class="gameResult__container">
+      <div class="gameResult__truthy">
+        <p class="gameResult__title">Правильные ответы:</p>
+        
+      </div>
+      <div class="gameResult__falsy">
+        <p class="gameResult__title">Неправильные ответы:</p>
+
+      </div>
+      <div class="gameResult__btns">
+        <button class="gameResult__btn gameResult__btn_main">Главаная страница</button>
+        <button class="gameResult__btn gameResult__btn_games">Сыграть еще раз</button>
+      </div>
+    </div>
+  `;
+
+  innerHtmlTemplateGameResultItem = `
+    <img class="gameResult__audio" src="./assets/svg/volumeUp.svg" alt="volumeUp">
+    <div class="gameResult__word"></div>
+    <p>-</p>
+    <div class="gameResult__translate"></div>
+  `;
+
+  contentURL: string;
+
+  componentElem: HTMLElement;
+
+  localStorageUser: AuthorizationContent;
+
+  constructor(contentURL: string) {
+    this.componentElem = document.createElement("div");
+    this.localStorageUser = LOCAL_STORAGE.getDataUser();
+    this.contentURL = contentURL;
+  }
+
+  createThisComponent() {
+    this.componentElem.innerHTML = this.innerHtmlTemplate;
+    this.componentElem.classList.add("gameResult");
+  }
+
+  createComponent() {
+    this.createThisComponent();
+  }
+
+  setThisListeners() {
+    const mainBtn = this.componentElem.querySelector(
+      ".gameResult__btn_main"
+    ) as HTMLButtonElement;
+    const gamesBtn = this.componentElem.querySelector(
+      ".gameResult__btn_games"
+    ) as HTMLButtonElement;
+
+    mainBtn.addEventListener("click", () => {
+      const mainNavBtn = document.querySelector(
+        "#mainBtn"
+      ) as HTMLButtonElement;
+      mainNavBtn.dispatchEvent(new Event("click"));
+    });
+    gamesBtn.addEventListener("click", () => {
+      const gamesNavBtn = document.querySelector(
+        "#gamesBtn"
+      ) as HTMLButtonElement;
+      gamesNavBtn.dispatchEvent(new Event("click"));
+    });
+  }
+
+  setListeners() {
+    this.setThisListeners();
+  }
+
+  async showComponent(
+    gameName: "sprint" | "audioCall",
+    answers: Array<{ result: boolean; wordContent: WordContent }>
+  ) {
+    this.createComponent();
+    this.setListeners();
+
+    if (localStorage.getItem("user")) {
+      await this.updateUserWords_OptionalProperty(gameName, answers);
+      await this.updateUserStatistics(gameName, answers);
+    }
+
+    const contentElem = document.querySelector(
+      ".main__container"
+    ) as HTMLElement;
+    contentElem.append(this.componentElem);
+
+    const truthyAnswerObjs = answers.filter(
+      (answerObj) => answerObj.result === true
+    );
+    const falsyAnswerObjs = answers.filter(
+      (answerObj) => answerObj.result === false
+    );
+
+    const truthyContainer = this.componentElem.querySelector(
+      ".gameResult__truthy"
+    ) as HTMLElement;
+    const falsyContainer = this.componentElem.querySelector(
+      ".gameResult__falsy"
+    ) as HTMLElement;
+
+    this.createGameResultItems(truthyAnswerObjs, truthyContainer);
+    this.createGameResultItems(falsyAnswerObjs, falsyContainer);
+  }
+
+  getBestTrueStreak(answers: { result: boolean; wordContent: WordContent }[]) {
+    if (answers.length === 0) {
+      return 0;
+    }
+
+    const streaks: Array<number> = [];
+    let streak = 0;
+    answers.forEach((answer, index) => {
+      if (answer.result === true) {
+        streak += 1;
+      }
+
+      if (index === answers.length - 1) {
+        streaks.push(streak);
+        return;
+      }
+
+      if (answer.result === false) {
+        streaks.push(streak);
+        streak = 0;
+      }
+    });
+
+    streaks.sort((a, b) => b - a);
+    return streaks[0];
+  }
+
+  async updateUserStatistics(
+    gameName: "sprint" | "audioCall",
+    answers: { result: boolean; wordContent: WordContent }[]
+  ) {
+    const currentDate = new Date().toLocaleDateString("en-US");
+    const currentBestStreak = this.getBestTrueStreak(answers);
+
+    const currentAmountOfRounds = answers.length;
+    let currentAmountOfTruthyAnswers = 0;
+    answers.forEach((answer) => {
+      if (answer.result === true) {
+        currentAmountOfTruthyAnswers += 1;
+      }
+    });
+
+    try {
+      const userStatistics = await statistic.getStatistics({
+        token: this.localStorageUser.token,
+        id: this.localStorageUser.userId,
+      });
+
+      if (currentDate in userStatistics.optional) {
+        const currentDateOptionalStatistics =
+          userStatistics.optional[currentDate];
+        const updatedOptionalStatistics = userStatistics.optional;
+
+        updatedOptionalStatistics[currentDate][gameName].totalCount +=
+          currentAmountOfRounds;
+        updatedOptionalStatistics[currentDate][gameName].trueCount +=
+          currentAmountOfTruthyAnswers;
+
+        if (
+          currentDateOptionalStatistics[gameName].bestStreak < currentBestStreak
+        ) {
+          updatedOptionalStatistics[currentDate][gameName].bestStreak =
+            currentBestStreak;
+        }
+
+        statistic.createStatistics({
+          token: this.localStorageUser.token,
+          id: this.localStorageUser.userId,
+          optional: updatedOptionalStatistics,
+        });
+      } else {
+        const updatedOptionalStatistics = userStatistics.optional;
+        updatedOptionalStatistics[currentDate] = {
+          sprint: {
+            bestStreak: gameName === "sprint" ? currentBestStreak : 0,
+            totalCount: gameName === "sprint" ? currentAmountOfRounds : 0,
+            trueCount: gameName === "sprint" ? currentAmountOfTruthyAnswers : 0,
+          },
+          audioCall: {
+            bestStreak: gameName === "audioCall" ? currentBestStreak : 0,
+            totalCount: gameName === "audioCall" ? currentAmountOfRounds : 0,
+            trueCount:
+              gameName === "audioCall" ? currentAmountOfTruthyAnswers : 0,
+          },
+        };
+
+        statistic.createStatistics({
+          token: this.localStorageUser.token,
+          id: this.localStorageUser.userId,
+          optional: updatedOptionalStatistics,
+        });
+      }
+    } catch {
+      const optionalStatistics: OptionalUserStatistics = {
+        [currentDate]: {
+          sprint: {
+            bestStreak: gameName === "sprint" ? currentBestStreak : 0,
+            totalCount: gameName === "sprint" ? currentAmountOfRounds : 0,
+            trueCount: gameName === "sprint" ? currentAmountOfTruthyAnswers : 0,
+          },
+          audioCall: {
+            bestStreak: gameName === "audioCall" ? currentBestStreak : 0,
+            totalCount: gameName === "audioCall" ? currentAmountOfRounds : 0,
+            trueCount:
+              gameName === "audioCall" ? currentAmountOfTruthyAnswers : 0,
+          },
+        },
+      };
+
+      statistic.createStatistics({
+        token: this.localStorageUser.token,
+        id: this.localStorageUser.userId,
+        optional: optionalStatistics,
+      });
+    }
+  }
+
+  async updateUserWords_OptionalProperty(
+    gameName: "sprint" | "audioCall",
+    answers: { result: boolean; wordContent: WordContent }[]
+  ) {
+    const userWordsContent = await usersWords.getUserWords({
+      token: this.localStorageUser.token,
+      id: this.localStorageUser.userId,
+    });
+
+    answers.forEach(async (answer) => {
+      const relatedUserWordContent = userWordsContent.find(
+        (userWordContent) => userWordContent.wordId === answer.wordContent.id
+      );
+
+      let isWordNew: boolean;
+      if (relatedUserWordContent) {
+        if (relatedUserWordContent.optional.dateWhenItBecameNew === false) {
+          isWordNew = true;
+        } else {
+          isWordNew = false;
+        }
+      } else {
+        isWordNew = true;
+      }
+
+      if (relatedUserWordContent) {
+        const updatedOptional: OptionalUserWord =
+          relatedUserWordContent.optional;
+
+        if (gameName === "sprint") {
+          updatedOptional.sprint.totalCount += 1;
+          if (answer.result === true) updatedOptional.sprint.trueCount += 1;
+        }
+
+        if (isWordNew === true) {
+          updatedOptional.dateWhenItBecameNew = new Date().toLocaleDateString(
+            "en-US"
+          );
+          updatedOptional.gameInWhichItBecameNew = `${gameName}`;
+        }
+
+        const currentDifficulty = relatedUserWordContent.difficulty;
+
+        if (currentDifficulty === "basic") {
+          updatedOptional.dateWhenItBecameLearned =
+            answer.result === true
+              ? new Date().toLocaleDateString("en-US")
+              : false;
+
+          usersWords.updateUserWord({
+            token: this.localStorageUser.token,
+            id: this.localStorageUser.userId,
+            wordId: answer.wordContent.id,
+            difficulty: answer.result === true ? "learned" : "hard",
+            optional: updatedOptional,
+          });
+        } else if (currentDifficulty === "hard") {
+          usersWords.updateUserWord({
+            token: this.localStorageUser.token,
+            id: this.localStorageUser.userId,
+            wordId: answer.wordContent.id,
+            difficulty: answer.result === true ? "basic" : "hard",
+            optional: updatedOptional,
+          });
+        } else if (currentDifficulty === "learned") {
+          usersWords.updateUserWord({
+            token: this.localStorageUser.token,
+            id: this.localStorageUser.userId,
+            wordId: answer.wordContent.id,
+            difficulty: answer.result === true ? "learned" : "basic",
+            optional: updatedOptional,
+          });
+        }
+      } else {
+        const optional: OptionalUserWord = {
+          dateWhenItBecameLearned:
+            answer.result === true
+              ? new Date().toLocaleDateString("en-US")
+              : false,
+          dateWhenItBecameNew: new Date().toLocaleDateString("en-US"),
+          gameInWhichItBecameNew: gameName,
+          sprint: {
+            totalCount: 0,
+            trueCount: 0,
+          },
+          audioCall: {
+            totalCount: 0,
+            trueCount: 0,
+          },
+        };
+
+        if (gameName === "sprint") {
+          optional.sprint.totalCount = 1;
+          optional.sprint.trueCount = answer.result === true ? 1 : 0;
+        }
+
+        usersWords.createUserWord({
+          token: this.localStorageUser.token,
+          id: this.localStorageUser.userId,
+          wordId: answer.wordContent.id,
+          difficulty: answer.result === true ? "learned" : "hard",
+          optional,
+        });
+      }
+    });
+  }
+
+  createGameResultItems(
+    answersObjs: {
+      result: boolean;
+      wordContent: WordContent;
+    }[],
+    hostElem: HTMLElement
+  ) {
+    answersObjs.forEach((answerObj) => {
+      const resultItem = document.createElement("div");
+      resultItem.classList.add("gameResult__item");
+      resultItem.innerHTML = this.innerHtmlTemplateGameResultItem;
+
+      const audioElem = resultItem.querySelector(
+        ".gameResult__audio"
+      ) as HTMLElement;
+      const wordElem = resultItem.querySelector(
+        ".gameResult__word"
+      ) as HTMLElement;
+      const translateElem = resultItem.querySelector(
+        ".gameResult__translate"
+      ) as HTMLElement;
+
+      audioElem.addEventListener("click", () => {
+        const audio = new Audio(
+          `${this.contentURL}${answerObj.wordContent.audio}`
+        );
+        audio.play();
+      });
+      wordElem.textContent = answerObj.wordContent.word;
+      translateElem.textContent = answerObj.wordContent.wordTranslate;
+
+      hostElem.append(resultItem);
+    });
+  }
+}
